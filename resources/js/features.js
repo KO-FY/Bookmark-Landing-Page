9a574f67f926e990f80ee39b19bf117aad84e798
asiedusolo <solomon.asiedu@amalitech.org>
2022-02-07 18:54:52 +0000

    starting js functionality

---
+++ resources/js/features.js
@@ -0,0 +1,20 @@
+const simple_book = document.querySelector(".simple_bookmark_container");
+const speedy_search = document.querySelector(".speedy_searching_container");
+const easy_share = document.querySelector(".easy_sharing_container");
+
+const heading = document.querySelector(".feature_sub_heading");
+const paragraph = document.querySelector(".feature_p");
+const btnText = document.querySelector(".feature_btn");
+
+// console.log(simple_book);
+// console.log(speedy_search);
+// console.log(easy_share);
+// console.log(heading);
+// console.log(paragraph);
+// console.log(btnText);
+
+simple_book.addEventListener("click", () => {});
+
+speedy_search.addEventListener("click", () => {});
+
+easy_share.addEventListener("click", () => {});
