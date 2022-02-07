a7a8f83ea69d5dc4112210e644d12463e4506238
asiedusolo <solomon.asiedu@amalitech.org>
2022-02-07 19:18:32 +0000

    displaying feature info dynamically

--- resources/js/features.js
+++ resources/js/features.js
@@ -4,17 +4,31 @@ const easy_share = document.querySelector(".easy_sharing_container");
 
 const heading = document.querySelector(".feature_sub_heading");
 const paragraph = document.querySelector(".feature_p");
-const btnText = document.querySelector(".feature_btn");
+// const btnText = document.querySelector(".feature_btn");
 
-// console.log(simple_book);
-// console.log(speedy_search);
-// console.log(easy_share);
-// console.log(heading);
-// console.log(paragraph);
-// console.log(btnText);
+simple_book.addEventListener("click", () => {
+  simple_book.classList.add("features_tab_hover");
+  speedy_search.classList.remove("features_tab_hover");
+  easy_share.classList.remove("features_tab_hover");
+  heading.innerText = "Bookmark in one click";
+  paragraph.innerText =
+    "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.";
+});
 
-simple_book.addEventListener("click", () => {});
+speedy_search.addEventListener("click", () => {
+  speedy_search.classList.add("features_tab_hover");
+  simple_book.classList.remove("features_tab_hover");
+  easy_share.classList.remove("features_tab_hover");
+  heading.innerText = "Intelligent search";
+  paragraph.innerText =
+    "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.";
+});
 
-speedy_search.addEventListener("click", () => {});
-
-easy_share.addEventListener("click", () => {});
+easy_share.addEventListener("click", () => {
+  easy_share.classList.add("features_tab_hover");
+  simple_book.classList.remove("features_tab_hover");
+  speedy_search.classList.remove("features_tab_hover");
+  heading.innerText = "Share your bookmarks";
+  paragraph.innerText =
+    "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.";
+});
