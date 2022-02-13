eb3c3e652b86bbb243af965d2b36f325a7ebc635
daniel-bontii <daniel.bontii@amalitech.org>
2022-02-13 10:21:40 +0000

    release 1 final styles, formatting, readme

--- resources/js/faq.js
+++ resources/js/faq.js
@@ -1,12 +1,11 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
-buttons.forEach( button =>{
-    button.addEventListener('click',()=>{
-        const faq = button.nextElementSibling;
-        const icon = button.children[1];
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const faq = button.nextElementSibling;
+    const icon = button.children[1];
 
-        faq.classList.toggle('show');
-        icon.classList.toggle('rotate');
-    })
-} )
-
+    faq.classList.toggle("show");
+    icon.classList.toggle("rotate");
+  });
+});
